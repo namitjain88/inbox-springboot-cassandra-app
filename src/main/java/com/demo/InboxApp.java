5b1aa37103cd52d60a71b1dd761c12bcec8e5d96
Namit Jain <namit.jain88@gmail.com>
2022-01-23 20:38:06 -0600

    Added Cassandra starter dependecy, astra connection properties and a secure budle (ignored in .gitignore) to securely connect to astra hosted db

--- src/main/java/com/demo/InboxApp.java
+++ src/main/java/com/demo/InboxApp.java
@@ -2,10 +2,14 @@ package com.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
 @RestController
@@ -20,6 +24,13 @@ public class InboxApp {
 		System.out.println(principal);
 		return principal.getAttribute("name");
 	}
-	
 
+	/**
+	 * This is necessary to have the Spring Boot app use the Astra secure bundle to connect to database
+	 */
+	@Bean
+	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties){
+		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+		return builder -> builder.withCloudSecureConnectBundle(bundle);
+	}
 }
