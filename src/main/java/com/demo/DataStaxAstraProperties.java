b949933d591dae7e9e5b43cbd98faf8f3cddd473
Namit Jain <namit.jain88@gmail.com>
2022-02-01 20:26:17 -0600

    Adding configuration annotation to resolve application start up failure

--- src/main/java/com/demo/DataStaxAstraProperties.java
+++ src/main/java/com/demo/DataStaxAstraProperties.java
@@ -1,9 +1,11 @@
 package com.demo;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.io.File;
 
+@Configuration
 @ConfigurationProperties(prefix = "datastax.astra")
 public class DataStaxAstraProperties {
 
