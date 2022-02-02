77114323346f5746d6fd648b7314529962e66326
Namit Jain <namit.jain88@gmail.com>
2022-02-01 21:51:48 -0600

    Showing inbox-page.html if user is logged in, index.html otherwise

---
+++ src/main/java/com/demo/inbox/contollers/InboxController.java
@@ -0,0 +1,19 @@
+package com.demo.inbox.contollers;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class InboxController {
+
+    @GetMapping("/")
+    public String homePage(@AuthenticationPrincipal OAuth2User principal){
+        if(principal == null || !StringUtils.hasText(principal.getAttribute("name"))){
+            return "index";
+        }
+        return "inbox-page";
+    }
+}
