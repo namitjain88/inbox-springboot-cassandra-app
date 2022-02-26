dcc3d7b54b4b75d566f938e4ed3f2fc5ea8738ae
Namit Jain <namit.jain88@gmail.com>
2022-02-25 20:09:55 -0600

    Fixed email list view to display emails in descending order

--- src/main/java/com/demo/inbox/emaillist/EmailListItemKey.java
+++ src/main/java/com/demo/inbox/emaillist/EmailListItemKey.java
@@ -1,6 +1,8 @@
 package com.demo.inbox.emaillist;
 
 import lombok.Data;
+
+import org.springframework.data.cassandra.core.cql.Ordering;
 import org.springframework.data.cassandra.core.cql.PrimaryKeyType;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyClass;
 import org.springframework.data.cassandra.core.mapping.PrimaryKeyColumn;
@@ -17,6 +19,6 @@ public class EmailListItemKey {
     @PrimaryKeyColumn(name="label", ordinal = 1, type = PrimaryKeyType.PARTITIONED)
     private String label;
 
-    @PrimaryKeyColumn(name="created_time_uuid", ordinal = 2, type = PrimaryKeyType.CLUSTERED)
+    @PrimaryKeyColumn(name="created_time_uuid", ordinal = 2, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
     private UUID timeUUID;
 }
